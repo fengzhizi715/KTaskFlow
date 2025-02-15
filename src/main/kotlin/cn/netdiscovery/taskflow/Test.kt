f2c84d30e1e6f16560f9c58079cba5ab978ff0f7
Tony Shen <fengzhizi715@126.com>
2025-02-15 22:06:03 +0800

    Update Test.kt

--- src/main/kotlin/cn/netdiscovery/taskflow/Test.kt
+++ src/main/kotlin/cn/netdiscovery/taskflow/Test.kt
@@ -13,23 +13,23 @@ import kotlinx.coroutines.*
 fun main() = runBlocking {
     val dag = dag {
         val task1 = task("1", "Task 1", 1) {
-            println("${System.currentTimeMillis()}, Task 1 start!")
+            LoggerProxy.getLogger().i("${System.currentTimeMillis()}, Task 1 start!")
             Thread.sleep(1000)
         }
         val task2 = task("2", "Task 2", 3) {
-            println("${System.currentTimeMillis()}, Task 2 start!")
+            LoggerProxy.getLogger().i("${System.currentTimeMillis()}, Task 2 start!")
             Thread.sleep(2000)
         }
         val task3 = task("3", "Task 3", 2) {
-            println("${System.currentTimeMillis()}, Task 3 start!")
+            LoggerProxy.getLogger().i("${System.currentTimeMillis()}, Task 3 start!")
             Thread.sleep(1500)
         }
         val task4 = task("4", "Task 4", type = TaskType.CPU) {
-            println("${System.currentTimeMillis()}, Task 4 start!")
+            LoggerProxy.getLogger().i("${System.currentTimeMillis()}, Task 4 start!")
             Thread.sleep(2000)
         }
         val task5 = task("5", "Task 5", type = TaskType.CPU) {
-            println("${System.currentTimeMillis()}, Task 5 start!")
+            LoggerProxy.getLogger().i("${System.currentTimeMillis()}, Task 5 start!")
             Thread.sleep(1000)
         }
 
@@ -40,11 +40,11 @@ fun main() = runBlocking {
 
         task5.dependsOn(task3,task4)
 
-        task1.successCallback = { println("${System.currentTimeMillis()}, Task 1 completed!") }
-        task2.successCallback = { println("${System.currentTimeMillis()}, Task 2 completed!") }
-        task3.successCallback = { println("${System.currentTimeMillis()}, Task 3 completed!") }
-        task4.successCallback = { println("${System.currentTimeMillis()}, Task 4 completed!") }
-        task5.successCallback = { println("${System.currentTimeMillis()}, Task 5 completed!") }
+        task1.successCallback = { LoggerProxy.getLogger().i("${System.currentTimeMillis()}, Task 1 completed!") }
+        task2.successCallback = { LoggerProxy.getLogger().i("${System.currentTimeMillis()}, Task 2 completed!") }
+        task3.successCallback = { LoggerProxy.getLogger().i("${System.currentTimeMillis()}, Task 3 completed!") }
+        task4.successCallback = { LoggerProxy.getLogger().i("${System.currentTimeMillis()}, Task 4 completed!") }
+        task5.successCallback = { LoggerProxy.getLogger().i("${System.currentTimeMillis()}, Task 5 completed!") }
     }
 
     println(generateDotFile(dag))
